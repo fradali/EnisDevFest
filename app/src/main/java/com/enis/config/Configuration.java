95a90279d682e8f9bdcef9f5154bc4a3abad9fd4
fradali <alifradi2011@gmail.com>
2015-11-21 23:56:58 +0100

    new

--- app/src/main/java/com/enis/config/Configuration.java
+++ app/src/main/java/com/enis/config/Configuration.java
@@ -4,6 +4,9 @@ package com.enis.config;
  * Created by ali on 21/11/15.
  */
 public class Configuration {
-    public static final String SERVER = "51.255.40.20";
-    //public static final String
+    //public static final String SERVER = "http://51.255.40.20";
+    public static final String SERVER = "http://51.255.40.20";
+    public static final String API_ACCOM="/accommodation";
+     public static final String API_POL="/police station";
+
 }
--- app/src/main/java/com/enis/enisdevfest/MainActivity.java
+++ app/src/main/java/com/enis/enisdevfest/MainActivity.java
@@ -3,13 +3,49 @@ package com.enis.enisdevfest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.enis.config.Configuration;
 import com.enis.robospice.BaseSampleSpiceActivity;
+import com.octo.android.robospice.persistence.DurationInMillis;
+import com.octo.android.robospice.persistence.exception.SpiceException;
+import com.octo.android.robospice.request.listener.RequestListener;
+import com.octo.android.robospice.request.simple.SimpleTextRequest;
 
-public class MainActivity extends BaseSampleSpiceActivity {
+import roboguice.util.temp.Ln;
+
+public class MainActivity extends BaseSampleSpiceActivity implements RequestListener<String> {
+
+    private SimpleTextRequest loremRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loremRequest = new SimpleTextRequest(Configuration.SERVER+Configuration.API_ACCOM);
+
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        setProgressBarIndeterminate(false);
+        setProgressBarVisibility(true);
+
+        getSpiceManager().execute(loremRequest, "txt", DurationInMillis.ONE_MINUTE, this);
+       // getSpiceManager().execute(imageRequest, EARTH_IMAGE_CACHE_KEY, 5 * DurationInMillis.ONE_MINUTE, new ImageRequestListener());
+    }
+
+    @Override
+    public void onRequestFailure(SpiceException spiceException) {
+        Ln.e("error de connexion");
+
+    }
+
+    @Override
+    public void onRequestSuccess(String s) {
+        Ln.i(s);
+
+
     }
 }
