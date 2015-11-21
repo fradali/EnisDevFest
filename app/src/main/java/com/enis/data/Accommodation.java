9ef894f1a0a0a867d6b15a433e83da01a02255c7
fradali <alifradi2011@gmail.com>
2015-11-22 00:16:42 +0100

    new

---
+++ app/src/main/java/com/enis/data/Accommodation.java
@@ -0,0 +1,79 @@
+package com.enis.data;
+
+/**
+ * Created by ali on 22/11/15.
+ */
+public class Accommodation {
+    private String address;
+    private String city;
+    private String typ;
+    private String location;
+    private String contact;
+    private String hotel;
+    private String name;
+    private String availability;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTyp() {
+        return typ;
+    }
+
+    public void setTyp(String typ) {
+        this.typ = typ;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(String hotel) {
+        this.hotel = hotel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(String availability) {
+        this.availability = availability;
+    }
+}
